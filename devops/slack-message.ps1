e4b0aa64ddd4bf31ae95928b5f780eaa5a872c54
marcinstanek <stanek.marcinp@gmail.com>
2020-05-29 21:17:12 +0200

    10-cypress adjustments

--- devops/slack-message.ps1
+++ devops/slack-message.ps1
@@ -1,4 +1,4 @@
-function Send-SlackMessage {
+function SendSlackMessage {
     param (
         [Parameter(Mandatory=$true, Position=0)]$Text,
         [Parameter(Mandatory=$true, Position=1)]$Url
@@ -18,4 +18,4 @@ Write-Host "Sending slack message for BUILD: $($env:BUILD_BUILDID)"
 $nl = "`r`n`r`n"
 $dnl = "$nl $nl"
 
-Send-SlackMessage "<!channel> $nl *$($env:REPORTS_PROJECTS_NAME) Test Results* $dnl BuildId: $($env:BUILD_BUILDID) $dnl Test results: https://dev.azure.com/$($env:REPORTS_PARTIAL_URL)/_build/results?buildId=$($env:BUILD_BUILDID)&view=ms.vss-test-web.build-test-results-tab $dnl Test analitycs: https://dev.azure.com/$($env:REPORTS_PARTIAL_URL)/_test/analytics?definitionId=1&contextType=build" $($env:WEBHOOK_URL)
+SendSlackMessage "<!channel> $nl *$($env:REPORTS_PROJECTS_NAME) Test Results* $dnl BuildId: $($env:BUILD_BUILDID) $dnl Test results: https://dev.azure.com/$($env:REPORTS_PARTIAL_URL)/_build/results?buildId=$($env:BUILD_BUILDID)&view=ms.vss-test-web.build-test-results-tab $dnl Test analitycs: https://dev.azure.com/$($env:REPORTS_PARTIAL_URL)/_test/analytics?definitionId=1&contextType=build" $($env:WEBHOOK_URL)
